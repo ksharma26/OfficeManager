04b2162d884e6e2533e9514c0e2f9ff11a445627
karans <karan.sharma@citrix.com>
2017-05-30 13:34:54 -0400

    commit 2: adding new training files and ability to ready docx files

--- Demo/train.py
+++ Demo/train.py
@@ -1,8 +1,7 @@
-import glob;
 import os;
 from sklearn.feature_extraction.text import TfidfVectorizer
 from pandas import DataFrame
-import slate;
+from docx import Document
 
 ### text vectorization--go from strings to lists of numbers
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -10,15 +9,18 @@ vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
 
 path = '.'
 
+
 def read_files(path):
     for root, dirs, files in os.walk(path, topdown=False):
         for dirName in dirs:
             if (not dirName .startswith('.')):
                 for filename in os.listdir(dirName):
-                    #if('.pdf' in filename):
-                    content = textwrap.(dirName + '/' + filename)
-                    #else:
-                    #    content = open(dirName + '/' + filename).read()
+                    if('docx' in filename):
+                        document = Document(dirName + '/' + filename)
+                        for para in document.paragraphs:
+                            content = para.text
+                    else:
+                        content = open(dirName + '/' + filename, encoding="latin-1").read()
                     yield dirName, content
 
 
@@ -31,20 +33,24 @@ def build_date_frame(path):
     return data_frame
 
 data = build_date_frame(path)
-print(data)
 
-# from sklearn.feature_extraction.text import CountVectorizer
-# count_vectorizer = CountVectorizer()
-# counts = count_vectorizer.fit_transform(data['text'].values)
-#
-#
-# from sklearn.naive_bayes import MultinomialNB
-#
-# classifier = MultinomialNB()
-# targets = data['class'].values
-# classifier.fit(counts, targets)
-#
-# examples = ['Social Security Number', "I'm going to attend the Linux users group tomorrow."]
-# example_counts = count_vectorizer.transform(examples)
-# predictions = classifier.predict(example_counts)
-# print(predictions)
+from sklearn.feature_extraction.text import CountVectorizer
+count_vectorizer = CountVectorizer()
+counts = count_vectorizer.fit_transform(data['text'].values)
+
+
+from sklearn.naive_bayes import MultinomialNB
+
+classifier = MultinomialNB()
+targets = data['class'].values
+classifier.fit(counts, targets)
+
+example_document = Document('C:/Users/karans/Desktop/Summers Brennan Interview.docx')
+for para in example_document.paragraphs:
+    example_content = para.text
+
+examples = [example_content]
+#examples = ['Social Security Number', "I'm going to attend the Linux users group tomorrow."]
+example_counts = count_vectorizer.transform(examples)
+predictions = classifier.predict(example_counts)
+print('OfficeManager Recommendation: ',  predictions)
